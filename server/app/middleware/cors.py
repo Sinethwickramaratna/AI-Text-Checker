f34839b7a8d040d26285a6c0ad162a4028de57a7
Sineth Wickramaratna <sinethwickramaratna@gmail.com>
2026-06-21 12:29:16 +0530

    feat: add CORS middleware to allow cross-origin requests from the production domain

--- server/app/middleware/cors.py
+++ server/app/middleware/cors.py
@@ -4,8 +4,7 @@ def add_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "https://veritextai.sinethwickramaratna.dev",
-            "http://localhost:3000"
+            "https://veritextai.sinethwickramaratna.dev"
         ],  # Allow all origins
         allow_credentials=True,
         allow_methods=["*"],  # Allow all HTTP methods
